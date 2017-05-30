7e10f99f600f20cdd7bf9a87029abc43869c9b2f
Brooke K <brookekingsland@users.noreply.github.com>
2017-05-30 12:41:09 -0500

    Did Users RCAV.

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,8 @@ Rails.application.routes.draw do
   get "/delete_photo/:id", :controller => "photos", :action => "destroy"
   #------------------------------
 
+  get "/users", :controller => "users", :action => "index"
+
   devise_for :users
   root to: "photos#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
